32ee12c0c3c822527a84806e7c63e5f95c078b68
mchenyuxiang <csyxchen@qq.com>
2017-02-04 16:41:46 +0800

    first finish

---
+++ PekingCplus/FirstWeek/sizeof_float.cc
@@ -0,0 +1,19 @@
+/**********************************************************
+ * Author        : mchenyuxiang
+ * Email         : mchenyuxiang@163.com
+ * Blog          : www.playdba.com
+ * Last modified : 2017-02-04 16:09
+ * Filename      : sizeof_float.cc
+ * Description   : 
+ * *******************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float a;
+	double b;
+	cin >> a >> b;
+	cout << sizeof(a) << " " << sizeof(b) << endl;
+	return 0;
+}
---
+++ PekingCplus/FirstWeek/sizeof_number.cc
@@ -0,0 +1,19 @@
+/**********************************************************
+ * Author        : mchenyuxiang
+ * Email         : mchenyuxiang@163.com
+ * Blog          : www.playdba.com
+ * Last modified : 2017-02-04 16:07
+ * Filename      : sizeof_number.cc
+ * Description   : 
+ * *******************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	short b;
+	cin >> a >> b;
+	cout << sizeof(a) << " " << sizeof(b) << endl;
+	return 0 ;
+}
