adc0cd580e30b60e2e6b490e934a8860f8ff2424
mchenyuxiang <csyxchen@qq.com>
2017-02-04 15:58:35 +0800

    output pic

---
+++ PekingCplus/FirstWeek/output_sample_pic.cc
@@ -0,0 +1,22 @@
+/**********************************************************
+ * Author        : mchenyuxiang
+ * Email         : mchenyuxiang@163.com
+ * Blog          : www.playdba.com
+ * Last modified : 2017-02-04 15:52
+ * Filename      : output_sample_pic.cc
+ * Description   : 
+ * *******************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char a;
+	cin >> a;
+	cout <<" "<<" "<<a<<endl;
+	cout <<" "<<a<<a<<a<<endl;
+	cout <<a<<a<<a<<a<<a<<endl;
+	cout <<" "<<a<<a<<a<<endl;
+	cout <<" "<<" "<<a<<endl;
+	return 0;
+}
