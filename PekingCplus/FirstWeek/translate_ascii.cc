8cd363a59353cf2c1725c54887a03a449981b72a
mchenyuxiang <csyxchen@qq.com>
2017-02-04 16:02:31 +0800

    translate_ascii

---
+++ PekingCplus/FirstWeek/translate_ascii.cc
@@ -0,0 +1,18 @@
+/**********************************************************
+ * Author        : mchenyuxiang
+ * Email         : mchenyuxiang@163.com
+ * Blog          : www.playdba.com
+ * Last modified : 2017-02-04 15:59
+ * Filename      : translate_ascii.cc
+ * Description   : 
+ * *******************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char a;
+	cin >> a;
+	cout << (int)a << endl;
+	return 0;
+}
