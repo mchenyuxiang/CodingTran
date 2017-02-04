7d7c74ee452e8a9e11827d02191e9c3b466d691c
mchenyuxiang <csyxchen@qq.com>
2017-02-04 15:51:40 +0800

    c_plus_plus

---
+++ PekingCplus/FirstWeek/a_plus_b.cc
@@ -0,0 +1,17 @@
+/**********************************************************
+ * Author        : mchenyuxiang
+ * Email         : mchenyuxiang@163.com
+ * Blog          : www.playdba.com
+ * Last modified : 2017-02-04 15:31
+ * Filename      : a_plus_b.cc
+ * Description   : 
+ * *******************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cin >> a >> b;
+	cout << a+b << endl;
+	return 0;
+}
---
+++ PekingCplus/FirstWeek/output_second_number.cc
@@ -0,0 +1,18 @@
+/**********************************************************
+ * Author        : mchenyuxiang
+ * Email         : mchenyuxiang@163.com
+ * Blog          : www.playdba.com
+ * Last modified : 2017-02-04 15:45
+ * Filename      : output_second_number.cc
+ * Description   : 
+ * *******************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+	cin >> a >> b >> c;
+	cout << b <<endl;
+	return 0;
+}
