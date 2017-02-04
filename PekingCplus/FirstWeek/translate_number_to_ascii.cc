49a5dfc64612b1a0a8173351021652cd104ae753
mchenyuxiang <csyxchen@qq.com>
2017-02-04 16:06:43 +0800

    translate

---
+++ PekingCplus/FirstWeek/translate_number_to_ascii.cc
@@ -0,0 +1,18 @@
+/**********************************************************
+ * Author        : mchenyuxiang
+ * Email         : mchenyuxiang@163.com
+ * Blog          : www.playdba.com
+ * Last modified : 2017-02-04 16:03
+ * Filename      : translate_number_to_ascii.cc
+ * Description   : 
+ * *******************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	cout << (char)a << endl;
+	return 0;
+}
