a1ad491eb3c253da47e336b53be9a6efc47650f4
mchenyuxiang <csyxchen@qq.com>
2017-03-02 13:40:01 +0800

    1.10

---
+++ cplusprimer/chapt01/1.10.cpp
@@ -0,0 +1,23 @@
+/**********************************************************
+ * Author        : mchenyuxiang
+ * Email         : mchenyuxiang@163.com
+ * Blog          : www.playdba.com
+ * Last modified : 2017-03-02 13:38
+ * Filename      : 1.10.cpp
+ * Description   : 
+ * *******************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int val = 10;
+	while(val >= 0){
+	
+		cout << val <<endl;
+		val --;
+	}
+}
