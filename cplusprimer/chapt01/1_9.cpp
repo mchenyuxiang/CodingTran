49d31d8262fc2c270a47ecd48bdaf5a544528eb4
mchenyuxiang <csyxchen@qq.com>
2017-03-02 13:38:09 +0800

    chapt 1.9

---
+++ cplusprimer/chapt01/1_9.cpp
@@ -0,0 +1,24 @@
+/**********************************************************
+ * Author        : mchenyuxiang
+ * Email         : mchenyuxiang@163.com
+ * Blog          : www.playdba.com
+ * Last modified : 2017-03-02 13:35
+ * Filename      : 1_9.cpp
+ * Description   : 
+ * *******************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int sum = 0;
+	int val = 50;
+	while( val <= 100){
+	
+		sum += val;
+		val ++;
+	}
+	cout << sum <<endl;
+}
