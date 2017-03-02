a7ebf5bef7870471b6a0725bb2940d96a6c29b43
mchenyuxiang <csyxchen@qq.com>
2017-03-02 13:34:18 +0800

    cpulsprimer

---
+++ cplusprimer/chapt01/1_8.cpp
@@ -0,0 +1,19 @@
+/**********************************************************
+ * Author        : mchenyuxiang
+ * Email         : mchenyuxiang@163.com
+ * Blog          : www.playdba.com
+ * Last modified : 2017-03-02 13:30
+ * Filename      : 1_8.cpp
+ * Description   : 
+ * *******************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "/*";
+	cout << "*/";
+	cout << "*/";
+}
