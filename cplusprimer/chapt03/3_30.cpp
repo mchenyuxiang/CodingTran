0e1575e9f8f87b29b992dc8d965417b15a715c4d
mchenyuxiang <csyxchen@qq.com>
2017-03-07 15:49:12 +0800

    数组

---
+++ cplusprimer/chapt03/3_30.cpp
@@ -0,0 +1,23 @@
+/**********************************************************
+ * Author        : mchenyuxiang
+ * Email         : mchenyuxiang@163.com
+ * Blog          : www.playdba.com
+ * Last modified : 2017-03-07 15:47
+ * Filename      : 3_30.cpp
+ * Description   : 
+ * *******************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int a[10];
+	for(int i=0;i<10;i++){
+	
+		a[i]=i;
+		cout << a[i] << endl;
+	}
+}
