51cc1c7d433408e461222198fedcfbfd3e9cc688
mchenyuxiang <mchenyuxiang@foxmail.com>
2017-11-16 02:23:42 +0800

    使用模板

---
+++ AlgorithmMuke/02-Sorting-Basic/02-Selection-Sort-Using-Templete/UsingTemplete.cpp
@@ -0,0 +1,32 @@
+/**
+ * File Name:   UsingTemplete.cpp
+ * Author:      mchenyuxiang
+ * Email:       csyuxiang1988@gmail.com
+ * CreateTime:  2017/11/16 2:22
+**/
+#include <iostream>
+
+using namespace std;
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main() {
+
+    int a[10] = {9, 3, 4, 5, 6, 7, 8, 2, 1, 10};
+    selectionSort(a, 10);
+    for (int i = 0; i < 10; i++)
+        cout << a[i] << " ";
+    cout << endl;
+
+    return 0;
+}
